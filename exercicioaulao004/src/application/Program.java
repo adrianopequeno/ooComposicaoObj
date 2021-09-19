155b65f1008fe10933a6c042078e2d0fdf978a20
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-09-19 20:23:00 -0300

    Create class client

--- exercicioaulao004/src/application/Program.java
+++ exercicioaulao004/src/application/Program.java
@@ -1,18 +1,27 @@
 package application;
 
-import entities.OrderItem;
-import entities.Product;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
 
 public class Program {
 	
-	public static void main(String arg[]) {
+	public static void main(String arg[]) throws ParseException {
 		
-		Product p = new Product("TV", 1000.00);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		OrderItem oi1 = new OrderItem(1, 1000.00, p);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println(oi1.getProduct().getName());
+		//Product p = new Product("TV", 1000.00);	
+		//OrderItem oi1 = new OrderItem(2, 1000.00, p);
+		Client cli = new Client("Adriano", "eu-a@hotmail.com", sdf.parse("13/05/1987"));
 		
+		System.out.println(cli);
 		
+		sc.close();
 	}
 }
